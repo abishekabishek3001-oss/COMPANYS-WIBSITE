cb4598c676e24faad50dd99627b45b7620f9279b
STUDENT <mohmmadrafath812@gmail.com>
2026-06-12 10:39:12 +0530

    vaeoiabler

---
+++ variablre.js
@@ -0,0 +1,11 @@
+// JavaScript code goes here
+var message = "Hello, World!"
+console.log(message)
+//let and const
+{
+let number1 = 10
+conslole.log(number1)
+}
+
+const number2 = 20
+console.log(number2)
